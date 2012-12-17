2cc492bd5e2dc93b5540f52d1bae85c3fc9ce212
Antonio <tokask@nimbus.(none)>
2012-12-17 13:24:44 +0100

    New Version

--- mag3110/mag3110.py
+++ mag3110/mag3110.py
@@ -33,6 +33,20 @@ ADDR_MAG = 0x0e   # I2C adrress of mag3110 device.
 BUS = 1           # Check your system - For Raspberry Rev.2 = 1
 CALTMP = 29.5     # In RTemp() offset temperature.
 
+# For testing purpose,three List of two val correction registers.
+x1 =  7
+x2 =  23
+y1 = -12
+y2 =  52
+z1 =  36
+z2 =  9
+
+valX = [x1, x2]
+valY = [y1, y2]
+valZ = [z1, z2]
+
+##################################################################
+
 class Raw_MAG3110(object):
 
    def __init__(self, bus_n=BUS, address=ADDR_MAG):
@@ -41,11 +55,30 @@ class Raw_MAG3110(object):
        self.address = address
        self.i2c.write8(CTRL_REG1, 0x00)    # Set STANDBY mode - Read DataSheet :-)
        self.i2c.write8(CTRL_REG2, MRST_RAW)
-       self.i2c.write8(CTRL_REG1, OS_DR)   # Operation modes 
+       self.i2c.write8(CTRL_REG1, OS_DR)   # Operation modes
+   
+   def setRawMode(self, action):
+       "Set=0 Raw with user correction or Set=1 Raw any correction)"
+       if (action == 0 or action == 1):
+         register = self.i2c.readU8(CTRL_REG2)
+         if (action != 0) & (action != (register & 0x20)):
+           TEMP_REG = self.i2c.readU8(CTRL_REG1)
+           self.i2c.write8(CTRL_REG1, 0x00)
+           self.i2c.write8(CTRL_REG2, (register | 0x20))
+           self.i2c.write8(CTRL_REG1, TEMP_REG)
+           return 1
+         elif action != (register & 0x20):
+           TEMP_REG = self.i2c.readU8(CTRL_REG1)
+           self.i2c.write8(CTRL_REG1, 0x00)
+           self.i2c.write8(CTRL_REG2, (register & 0xdf))
+           self.i2c.write8(CTRL_REG1, TEMP_REG)
+           return 2
+       else:
+         return 3
 
    def StaAct(self, action):
        "Put mag3110 in ACTIVE (1) or STANDBY (0) mode"
-       if action != 0 | action != 1:
+       if (action == 0 | action == 1):
          return False 
        register = self.i2c.readU8(CTRL_REG1)
        if (action!=0) & (action != (register & 0x01)):
@@ -57,27 +90,27 @@ class Raw_MAG3110(object):
        else:
          return False       
     
-   def rTemp(self):
+   def rdTemp(self):
        "Read temp from register 8-bit signed"
        temp = self.i2c.readS8(DIE_TEMP)
        return (temp + CALTMP)
 
-   def read_X16(self):
+   def rdX16(self):
        "Read X mag from register 16-bit signed"
        Xmag = self.i2c.readS16(OUT_X_MSB)
        return Xmag
 
-   def read_Y16(self):
+   def rdY16(self):
        "Read Y mag from register 16-bit signed"
        Ymag = self.i2c.readS16(OUT_Y_MSB)
        return Ymag
 
-   def read_Z16(self):
+   def rdZ16(self):
        "Read Z mag from register 16-bit signed"
        Zmag = self.i2c.readS16(OUT_Z_MSB)
        return Zmag
 
-   def CurrSysMode(self):
+   def sysMode(self):
        "Read Current system mode"
        sys = self.i2c.readU8(SYSMOD)
        if sys == 0:
@@ -88,53 +121,55 @@ class Raw_MAG3110(object):
          return "10: ACTIVE mode, non-RAW user-corrected data."
        return False
    
-   def SetX16_Offset(self, list):
+   def setX16_Offset(self, list):
        try:
          self.i2c.bus.write_i2c_block_data(self.address, OFF_X_MSB, list)
        except IOError, err:
          print "Error accessing 0x%02X" % self.address
          return -1
 
-   def readX16_Offset(self):
+   def rdX16_Offset(self):
        "Read X mag User Offset 16-bit signed"
        X_offset = self.i2c.readS16(OFF_X_MSB)
        return X_offset  
 
-   def SetY16_Offset(self, list):
+   def setY16_Offset(self, list):
        try:
          self.i2c.bus.write_i2c_block_data(self.address, OFF_Y_MSB, list)
        except IOError, err:
          print "Error accessing 0x%02X" % self.address
          return -1
 
-   def readY16_Offset(self):
+   def rdY16_Offset(self):
        "Read Y mag User Offset 16-bit signed"
        Y_offset = self.i2c.readS16(OFF_Y_MSB)
        return Y_offset
 
-   def SetZ16_Offset(self, list):
+   def setZ16_Offset(self, list):
        try:
          self.i2c.bus.write_i2c_block_data(self.address, OFF_Z_MSB, list)
        except IOError, err:
          print "Error accessing 0x%02X" % self.address
          return -1
 
-   def readZ16_Offset(self):
+   def rdZ16_Offset(self):
        "Read Z mag User Offset 16-bit signed"
        Z_offset = self.i2c.readS16(OFF_Z_MSB)
        return Z_offset
 
-   def read_CTRLREG(self, reg):
+   def rdCTRLREG(self, reg):
        "Read Control register: 1 for REG1 and 2 for REG2."
        if reg == 1:
          ctrl1 = self.i2c.readU8(CTRL_REG1)
          return ctrl1
        elif reg == 2:
          ctrl2 = self.i2c.readU8(CTRL_REG2)
-         if ctrl2 == 16:
-           return "Mag_RST - Magnetic Sensor Reset"
-         elif ctrl2 == 32:
-           return "RAW mode - No user data correction"
+         if (ctrl2 & 16):
+           return ctrl2, "Mag_RST - Magnetic Sensor Reset"
+         elif (ctrl2 & 32):
+           return ctrl2, "RAW mode - No user data correction"
+         elif ~(ctrl2 & 32):
+           return ctrl2, "RAW mode - User data correction"
          else: 
            return ctrl2 
        return False
